513262d3d2b3ca00860020afcb5b510a08f29eeb
HakasOD <hak.dimozantos@gmail.com>
2024-11-09 19:05:57 +1100

    Add theme toggler

--- src/index.js
+++ src/index.js
@@ -4,6 +4,23 @@ document.addEventListener("DOMContentLoaded", () => {
 	const rootElement = document.documentElement;
 	const themeKey = "theme";
 
+	initiliseTheme(themeKey, rootElement);
+
+	toggleThemeButton.addEventListener("click", () => {
+		toggleTheme(rootElement, themeKey);
+		updateCheckboxVisual(rootElement);
+	});
+});
+
+function toggleTheme(rootElement, themeKey) {
+	const currentTheme = rootElement.getAttribute("data-theme");
+	const newTheme = currentTheme === "light" ? "dark" : "light";
+
+	rootElement.setAttribute("data-theme", newTheme);
+	localStorage.setItem(themeKey, newTheme);
+}
+
+function initiliseTheme(themeKey, rootElement) {
 	const storedTheme = localStorage.getItem(themeKey);
 
 	if (storedTheme) {
@@ -11,13 +28,20 @@ document.addEventListener("DOMContentLoaded", () => {
 	} else {
 		rootElement.setAttribute("data-theme", "light");
 	}
+}
 
-	toggleThemeButton.addEventListener("click", () => {
-        const currentTheme = rootElement.getAttribute("data-theme");
-        console.log(currentTheme)
-		const newTheme = currentTheme === "light" ? "dark" : "light";
-        console.log(newTheme)
-		rootElement.setAttribute("data-theme", newTheme);
-		localStorage.setItem(themeKey, newTheme);
-	});
-});
+function updateCheckboxVisual(rootElement) {
+	const checkbox = document.querySelector("#theme-toggler-checkbox");
+	const theme = rootElement.getAttribute("data-theme");
+
+	if (theme === "light") {
+		checkbox.checked = false;	
+	} else {
+		checkbox.checked = true;
+	}
+}
+
+function preventCheckboxClick(event) {
+	event.stopPropagation();
+	event.preventDefault();
+}
